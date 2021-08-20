236d5e2855c7660202e6692c8e817e18bfa34e8b
Nebojsa Borocki <nborocki@gmail.com>
2021-08-20 10:35:58 +0200

    delete env and change yml

--- env/Lib/site-packages/rflint/exceptions.py
+++
@@ -1,8 +0,0 @@
-'''
-This file defines custom exceptions used by robotframework-lint
-
-'''
-
-class UnknownRuleException(Exception):
-    def __init__(self, rulename):
-        super(UnknownRuleException, self).__init__("unknown rule: '%s'" % rulename)
